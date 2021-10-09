e9639a0fac5d604717be808d6e2b23ce4331b8cb
Nikita Kuzin <nickoskuzin@gmail.com>
2021-10-09 12:11:22 +0200

    add the calculation game

---
+++ bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import calcGame from '../src/games/calc_game.js';
+
+console.log('Welcome to the Brain Games!');
+calcGame();
---
+++ src/games/calc_game.js
@@ -0,0 +1,71 @@
+import {
+  cons, car, cdr, toString, isPair,
+} from '@hexlet/pairs';
+
+import {
+  correctAnswersToWin, getUserName, greetUser,
+  getRandomInt, getRandomOperation, askUser, isAnswerCorrect,
+} from '../index.js';
+
+const calculateCorrectAnswer = (expr) => {
+  let answer = 0;
+
+  if (!isPair(expr)) {
+    return null;
+  }
+
+  const operation = car(expr);
+  const num1 = car(cdr(expr));
+  const num2 = cdr(cdr(expr));
+
+  if (operation === '+') {
+    answer = num1 + num2;
+  } else if (operation === '-') {
+    answer = num1 - num2;
+  } else if (operation === '*') {
+    answer = num1 * num2;
+  }
+
+  return answer.toString();
+};
+
+const prettifyExpression = (expr) => {
+  if (!isPair(expr)) {
+    return null;
+  }
+
+  const operation = car(expr);
+  const firstNumber = car(cdr(expr)).toString();
+  const secondNumber = cdr(cdr(expr)).toString();
+  return `${firstNumber} ${operation} ${secondNumber}`;
+};
+
+const calcGame = () => {
+  const userName = getUserName();
+  greetUser(userName);
+  console.log('What is the result of the expression?');
+
+  let correctAnswerCounter = 0;
+
+  while (correctAnswerCounter < correctAnswersToWin) {
+    const firstNumberToAsk = getRandomInt();
+    const secondNumberToAsk = getRandomInt();
+    const operation = getRandomOperation();
+    const expression = cons(operation, cons(firstNumberToAsk, secondNumberToAsk));
+    const userAnswer = askUser(prettifyExpression(expression));
+    const calculatedAnswer = calculateCorrectAnswer(expression);
+
+    if (!isAnswerCorrect(calculatedAnswer, userAnswer)) {
+      console.log(`${userAnswer} is the wrong answer :(. The correct answer was ${calculatedAnswer}`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    correctAnswerCounter += 1;
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default calcGame;
