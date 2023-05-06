c1936cbee1d9d34be26bd7a5f44e48c4a54cc8a1
Nikita Kuzin <nickoskuzin@gmail.com>
2023-05-06 12:54:30 +0200

    rewrite the brain-even game with new logic

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,37 +1,12 @@
-import readlineSync from 'readline-sync';
-import getUserName from './cli.js';
+import cons from '@hexlet/pairs';
+import { getRandomInt } from '../index.js';
 
-const getRandomInt = () => Math.round(Math.random() * 100);
+export const instruction = 'Answer "yes" if the number is even, otherwise answer "no"';
 
 const isEvenExplicit = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
-const askUser = () => {
-  const answer = readlineSync.question('Your answer: ');
-  return answer;
+export const createGameContent = () => {
+  const numberToAsk = getRandomInt(100);
+  const correctAnswer = isEvenExplicit(numberToAsk);
+  return cons(numberToAsk, correctAnswer);
 };
-
-const evenGame = () => {
-  const userName = getUserName();
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-
-  const correctGuessesToWin = 3;
-  let userCorrectGuesses = 0;
-
-  while (userCorrectGuesses < correctGuessesToWin) {
-    const numberToAsk = getRandomInt();
-    console.log(`Question: ${numberToAsk}`);
-    const userAnswer = askUser();
-    const correctAnswer = isEvenExplicit(numberToAsk);
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`${userAnswer} is the wrong answer :(. The correct answer was ${correctAnswer}`);
-      console.log(`Let's try again ${userName}`);
-      return;
-    }
-    console.log('Correct!');
-    userCorrectGuesses += 1;
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-export default evenGame;
